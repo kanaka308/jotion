7c887f57f1c0ae75dfdbbbf0dff8eabe4be4ad48
unknown <shivananddhanagond48@gmail.com>
2024-04-02 09:40:50 +0530

    Marketing Page Completed

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-export default function Home() {
+import Heading from "./_components/heading";
+import Heroes from "./_components/heroes";
+import Footer from "./_components/footer";
+const MarketingPage = () => {
   return (
-    <>
-    <div className="">
-      <Button size="lg">Hello</Button>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:flex-start text-center gap-y-8 flex-1 px-6 pb-10 dark:bg-[#1F1F1F]">
+        <Heading/>
+        <Heroes/>
+      </div>
+      <Footer/>
     </div>
-    </>
   )
 }
+
+export default MarketingPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,8 +28,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-red-500`}>{children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange storageKey="jotion-theme-2"
+        >{children}</ThemeProvider>
+        
       </body>
     </html>
   );
